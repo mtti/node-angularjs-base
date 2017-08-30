68042e49a68d27d6d59b4cd9df90e2dd527803da
mtti <mtti@users.noreply.github.com>
2017-08-30 20:27:03 +0300

    Add winston for logging

--- server/config.js
+++ server/config.js
@@ -1,11 +1,21 @@
 import Sequelize from 'sequelize';
+import winston from 'winston';
 
+const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 3000;
-const dbUri = process.env.DATABASE_URL || 'postgres://developer:password@localhost:5432/database';
 
+winston.setLevels(winston.config.syslog.levels);
+if (env === 'production') {
+  winston.level = 'info';
+} else {
+  winston.level = 'debug';
+}
+
+const dbUri = process.env.DATABASE_URL || 'postgres://developer:password@localhost:5432/database';
 const sequelize = new Sequelize(dbUri);
 
 const config = {
+  env,
   sequelize,
   port,
 };
